8edf2012d83a5f65f432675cfff1c5ee0a29f19a
shalamike <m.shalaby1994@gmail.com>
2023-03-01 17:04:34 +0000

    salary service included aswell

--- src/main/java/com/sparta/burgerspring/model/repositories/SalaryRepository.java
+++ src/main/java/com/sparta/burgerspring/model/repositories/SalaryRepository.java
@@ -3,6 +3,11 @@ package com.sparta.burgerspring.model.repositories;
 import com.sparta.burgerspring.model.entities.Salary;
 import com.sparta.burgerspring.model.entities.SalaryId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SalaryRepository extends JpaRepository<Salary, SalaryId> {
+
+
 }
