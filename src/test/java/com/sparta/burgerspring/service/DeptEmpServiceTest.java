04527deea7176ce83394c9433462e1513ef580cd
Oliver Norgate <oliverpnorgate@gmail.com>
2023-03-02 17:16:29 +0000

    created a test to check for non null and also added a test to check that the size of finance department ina  given year is equal to expected result

--- src/test/java/com/sparta/burgerspring/service/DeptEmpServiceTest.java
+++ src/test/java/com/sparta/burgerspring/service/DeptEmpServiceTest.java
@@ -1,6 +1,7 @@
 package com.sparta.burgerspring.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,23 @@ class DeptEmpServiceTest {
     @Autowired
     DeptEmpService deptEmpService;
 
+    String testCase;
+    @BeforeEach
+    void setup() {
+        testCase = deptEmpService.getSizeOfDepartmentInGivenYear("finance", LocalDate.of(1990,01,01), LocalDate.of(1990, 12, 31));
+    }
+
+
+    @Test
+    @DisplayName("Given the department is finance and a from and to date is set, returns a non-null value")
+    void givenValidParams_TestForNonNullOne(){
+        assertTrue(testCase != null);
+    }
+
+
     @Test
     @DisplayName("Given department finance, between 1990-01-01 and 1990-12-31, returns size of 15")
     void givenDepartmentFinanceBetweenGivenYear_ReturnsSizeOf15() {
-        assertEquals("The size of finance is 15", deptEmpService.getSizeOfDepartmentInGivenYear("finance", LocalDate.of(1990,01,01), LocalDate.of(1990, 12, 31)));
+        assertEquals("The size of finance is 15", testCase);
     }
 }
