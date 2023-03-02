b0abd11b6afbeb508ba846919ae6d1a2fb6b9774
Oliver Norgate <oliverpnorgate@gmail.com>
2023-03-02 12:10:27 +0000

    Added code in deptemprepo using JDQL to find count of employees in dept, also created deptempservice

--- src/main/java/com/sparta/burgerspring/model/repositories/DeptEmpRepository.java
+++ src/main/java/com/sparta/burgerspring/model/repositories/DeptEmpRepository.java
@@ -3,6 +3,17 @@ package com.sparta.burgerspring.model.repositories;
 import com.sparta.burgerspring.model.entities.DeptEmp;
 import com.sparta.burgerspring.model.entities.DeptEmpId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface DeptEmpRepository extends JpaRepository<DeptEmp, DeptEmpId> {
+
+    @Query("SELECT COUNT(e) FROM DeptEmp e INNER JOIN Department d" +
+            " ON e.deptNo = d.id WHERE d.deptName = :deptName" +
+            " AND e.fromDate >= :fromDate AND e.toDate <= :toDate")
+    Integer getSizeOfDepartment(String deptName, LocalDate fromDate, LocalDate toDate);
+
 }
