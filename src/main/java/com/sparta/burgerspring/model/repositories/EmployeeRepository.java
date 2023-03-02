9ebf637056ac90e5e05ea55f824b277f6690ffb7
Sina Naseri <naseri.sina@hotmail.com>
2023-03-02 10:36:50 +0000

    finish the find employeeByLastName Done

--- src/main/java/com/sparta/burgerspring/model/repositories/EmployeeRepository.java
+++ src/main/java/com/sparta/burgerspring/model/repositories/EmployeeRepository.java
@@ -2,6 +2,13 @@ package com.sparta.burgerspring.model.repositories;
 
 import com.sparta.burgerspring.model.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e FROM Employee e WHERE e.lastName = ?1")
+    List<Employee> findByLastName(String lastName);
+
 }
