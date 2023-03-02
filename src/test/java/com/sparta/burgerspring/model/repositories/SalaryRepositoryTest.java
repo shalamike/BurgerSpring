5d7660e5be7a684bc93416c5c5c4ce84f43de6ad
shalamike <m.shalaby1994@gmail.com>
2023-03-02 17:50:06 +0000

    added additional method to find highest salary earned by given employee, also tested to see if list of employees earning above a given salary is not null and and tested method to find highest salary of given employee

--- src/test/java/com/sparta/burgerspring/model/repositories/SalaryRepositoryTest.java
+++ src/test/java/com/sparta/burgerspring/model/repositories/SalaryRepositoryTest.java
@@ -1,15 +1,31 @@
 package com.sparta.burgerspring.model.repositories;
 
+import com.sparta.burgerspring.service.SalaryService;
 import org.junit.jupiter.api.*;
 import org.mockito.internal.runners.RunnerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class SalaryRepositoryTest {
+
+    @Autowired
+    SalaryRepository salaryRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("testing if given id of 10032 eanred a max of 69539")
+    void testEmployeeEarnsMaxOf69539(){
+        assertTrue(salaryRepository.highestSalaryOfGivenEmployeeId(10032) == 69539);
+    }
 
 
 }
