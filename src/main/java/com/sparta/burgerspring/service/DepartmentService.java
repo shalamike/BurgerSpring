64f7a8008b94c88c439bd2a4e18f1e6bd3a82948
Miguel <miguel.camilleri1@gmail.com>
2023-03-03 17:13:54 +0000

    Refactored both methods in DepartmentService and added test cases

--- src/main/java/com/sparta/burgerspring/service/DepartmentService.java
+++ src/main/java/com/sparta/burgerspring/service/DepartmentService.java
@@ -3,7 +3,9 @@ package com.sparta.burgerspring.service;
 import com.sparta.burgerspring.model.repositories.DepartmentRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,12 +17,29 @@ public class DepartmentService {
     }
 
 
+    public String getAvgSalByDeptNameAndDate(String deptName, LocalDate date) {
+        List<String> depts = new ArrayList<>(List.of("Customer Service", "Development", "Finance", "Human Resources", "Marketing", "Production", "Quality Management", "Research", "Sales"));
+        LocalDate currentDate = LocalDate.now();
+        if (deptName == null || date == null) {
+            throw new NullPointerException("Arguments can't be null");
+        }
 
-    public String getAvgSalByDeptNameAndDate(String deptName, LocalDate date){
-        return "Average salary for the " + deptName + " department " + "during '" + date + "' is " + departmentRepository.getListOfSalariesByDept(deptName, date);
+
+        if (depts.contains(deptName)){
+            if (date.isBefore(LocalDate.of(1985,1,1)) || date.isAfter(currentDate)) {
+                return "Invalid date selection: Please enter a date from 1985-01-01 to " + currentDate + ".";
+            } else {
+                return "Average salary for the " + deptName + " department " + "during '" + date + "' is " + departmentRepository.getListOfSalariesByDept(deptName, date);
+            }
+        }
+        else{
+            return "Department '" + deptName + "' not present in Database";
+        }
+
+
     }
 
-    public String getListOfDeptNamesByEmp(String firstName, String lastName){
+    public String getListOfDeptNamesByEmp(String firstName, String lastName) {
         if (firstName == null || lastName == null) {
             throw new NullPointerException("Arguments can't be null");
         }
--- src/test/java/com/sparta/burgerspring/service/DepartmentServiceTest.java
+++ src/test/java/com/sparta/burgerspring/service/DepartmentServiceTest.java
@@ -4,8 +4,14 @@ import com.sparta.burgerspring.model.repositories.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,20 +25,22 @@ class DepartmentServiceTest {
     DepartmentRepository departmentRepository;
 
     String testForFinance;
-    String testForNull;
+    String testForNotNull;
     String testForNonExistent;
+    LocalDate currentDate;
 
     @BeforeEach
     void setup() {
-        testForNull = departmentService.getListOfDeptNamesByEmp("Uri", "Rullman");
+        testForNotNull = departmentService.getListOfDeptNamesByEmp("Uri", "Rullman");
         testForFinance = departmentService.getListOfDeptNamesByEmp("Uri", "Rullman");
         testForNonExistent = departmentService.getListOfDeptNamesByEmp("Non", "Existent");
+        currentDate = LocalDate.now();
     }
 
     @Test
     @DisplayName("Given a first name and last name, returns a non-null value")
     void givenValidParams_ReturnsNonNull() {
-        assertTrue(testForNull != null);
+        assertTrue(testForNotNull != null);
     }
 
     @Test
@@ -59,17 +67,39 @@ class DepartmentServiceTest {
         assertEquals("Invalid input: Employee not found.", testForNonExistent);
     }
 
+
     @Test
-    @DisplayName("Check that method returns list greater than 0 if employee is found")
-    void givenThatEmployeeExistsReturnNameAndDept() {
-        assertTrue(departmentRepository.getListOfDeptsByName("Uri", "Rullman").size() > 0);
+    @DisplayName("Check that exception is thrown if parameters are null for getListOfDeptsByName")
+    void givenThatparametersAreNullThrowException() {
+        assertThrowsExactly(NullPointerException.class, () -> departmentService.getListOfDeptNamesByEmp(null, null));
     }
 
     @Test
-    @DisplayName("Check that method returns list with 2 elements for Uri Rullman")
-    void givenThatEmployeeExistsReturnCorrectListSize() {
-        assertTrue(departmentRepository.getListOfDeptsByName("Uri", "Rullman").size() == 2);
+    @DisplayName("Check that correct Department name is given in getAvgSalByDeptNameAndDate method")
+    void givenThatDeptNameIsIncorrectReturnAppropriateString() {
+        assertEquals("Department 'Engineering' not present in Database", departmentService.getAvgSalByDeptNameAndDate("Engineering", LocalDate.of(2000,01,01)));
     }
+
+    @Test
+    @DisplayName("Check that NullPointerException is thrown if an argument is null in getAvgSalByDeptNameAndDate method")
+    void givenThatArgIsNullThrowException() {
+        assertThrowsExactly(NullPointerException.class, () -> departmentService.getAvgSalByDeptNameAndDate(null, null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1980-01-01", "2024-01-01"})
+    @DisplayName("Check that date given is in the past and after hire date of first employee in getAvgSalByDeptNameAndDate method")
+    void givenThatDateIsIncorrectReturnAppropriateString(LocalDate date) {
+        assertEquals("Invalid date selection: Please enter a date from 1985-01-01 to " + currentDate + ".", departmentService.getAvgSalByDeptNameAndDate("Finance", date));
+    }
+
+    @Test
+    @DisplayName("Check that data is returned if correct date and dept name is given in getAvgSalByDeptNameAndDate method")
+    void givenThatArgsAreValidReturnData() {
+        assertEquals("Average salary for the Sales department during '2000-01-01' is 83416.7841", departmentService.getAvgSalByDeptNameAndDate("Sales", LocalDate.of(2000,01,01)));
+    }
+
+
 
 
 }
