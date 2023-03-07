2b257b7e89dd58e1dd1f81b4d8a25ea95df5cbe8
shalamike <m.shalaby1994@gmail.com>
2023-03-07 17:02:10 +0000

    added some comments in burger spring to try detect if any one has done accidentally pushed their code

--- src/main/java/com/sparta/burgerspring/BurgerSpringApplication.java
+++ src/main/java/com/sparta/burgerspring/BurgerSpringApplication.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BurgerSpringApplication {
-
-
+////this is some comment if work is done here there should be a merge conflict
+    public static void main(String[] args){
+        SpringApplication.run(BurgerSpringApplication.class, args);
+    }
+//this is another comment, if work is done here there will be a merge conflict
 }
