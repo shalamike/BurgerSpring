d5b85a3c0d7f1bc9fa967139745474a0c07efd98
shalamike <m.shalaby1994@gmail.com>
2023-03-07 17:52:03 +0000

    created a boilerplate salary controller class with some unimplemented methods

--- src/main/java/com/sparta/burgerspring/controller/SalaryController.java
+++ src/main/java/com/sparta/burgerspring/controller/SalaryController.java
@@ -1,4 +1,33 @@
 package com.sparta.burgerspring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.burgerspring.service.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SalaryController {
+
+    private final SalaryService salaryService;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public SalaryController(SalaryService salaryService, ObjectMapper objectMapper){
+        this.salaryService = salaryService;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(value = "/salary/{id}")//this will return a single employee salary
+    public ResponseEntity<String> getSalaryById(@RequestParam Integer id){
+        return null;
+    }
+
+    @GetMapping(value = "/salaries")//this will return a list of employees
+    public ResponseEntity<String> getEmployeesEarningAboveSalary(Integer salary){
+        return null;
+    }
 }
