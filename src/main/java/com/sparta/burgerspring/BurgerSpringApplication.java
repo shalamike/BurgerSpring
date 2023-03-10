8513bc64ec5fed38cd1970a595e6bd2d741341d9
Honghao Zheng <hxz157@cs.bham.ac.uk>
2023-03-10 12:27:10 +0000

    CRUD without testing

--- src/main/java/com/sparta/burgerspring/BurgerSpringApplication.java
+++ src/main/java/com/sparta/burgerspring/BurgerSpringApplication.java
@@ -6,9 +6,11 @@ import com.sparta.burgerspring.model.repositories.DepartmentRepository;
 import com.sparta.burgerspring.model.repositories.DeptEmpRepository;
 import com.sparta.burgerspring.model.repositories.EmployeeRepository;
 
+import com.sparta.burgerspring.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.slf4j.LoggerFactoryFriend;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,8 +21,19 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 public class BurgerSpringApplication {
+    private Logger logger= LoggerFactory.getLogger(BurgerSpringApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(BurgerSpringApplication.class, args);
+    }
+//    @Bean
+//    public CommandLineRunner run(EmployeeRepository employeeRepository){
+//
+//    return args -> {
+//        logger.info(employeeRepository.findByFirstNameAndAndLastName("Parto","Bamford").toString());
+//    };
+//    }
+
+
     }
 
 
@@ -53,4 +66,3 @@ public class BurgerSpringApplication {
 //}
 
 
-}
--- src/test/java/com/sparta/burgerspring/controller/EmployeeControllerTest.java
+++ src/test/java/com/sparta/burgerspring/controller/EmployeeControllerTest.java
@@ -1,4 +1,39 @@
 package com.sparta.burgerspring.controller;
 
+import com.sparta.burgerspring.model.repositories.EmployeeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class EmployeeControllerTest {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeController employeeController;
+
+//    @Test
+//    @DisplayName("Check the status code of a response")
+//    void checkTheStatusCodeOfAResponse() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8080/employees", String.class);
+//        System.out.println(responseEntity.getBody());
+//        assertEquals(200,responseEntity.getStatusCode().value());
+//    }
+
+    @Test
+    public void CheckCorrectEmployeeFoundById(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(
+                "http://localhost:8080/employee/10001", String.class);
+        System.out.println(responseEntity.getBody().toString());
+        assertEquals(employeeRepository.findById(10001).toString(),responseEntity.getBody().toString());
+
+
+    }
 }
